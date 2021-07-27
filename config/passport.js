f9552177cac0a68ae948d5d2b14df6c715e58a28
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-26 22:21:23 -0500

    add nodemon

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,11 @@
 const passport = require('passport')
 const User = require('../models/user')
-const FacebookStrategy = require('passport-facebook').Strategy;
+const FacebookStrategy = require('passport-facebook');
 
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: process.env.ON_PROD == '1' ? proccess.env.FACEBOOK_CALLBACK_URL : 'http://localhost:3000/facebookLogin/callback'
+  callbackURL: process.env.ON_PROD == '1' ? proccess.env.FACEBOOK_CALLBACK_URL : 'http://localhost:3000/'
 }, (accessToken, refreshToken, profile, done) => {
   console.log(profile)
 }))
