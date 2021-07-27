f38cef9a3835a4045e757b55bbe1306282f9f899
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-27 11:25:26 -0500

    add

--- config/passport.js
+++ config/passport.js
@@ -1,45 +1,65 @@
-const passport = require('passport')
-const User = require('../models/user')
-const FacebookStrategy = require('passport-facebook');
+const passport = require("passport");
+const User = require("../models/user");
+const FacebookStrategy = require("passport-facebook");
 
-passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-passport.serializeUser(function(user, done) {
-  done(null, user._id)
-})
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    if (!err) done(null, user)
-    else done(err, null)
-  })
-})
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (!err) done(null, user);
+    else done(err, null);
+  });
+});
 
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: process.env.ON_PROD == '1' ? process.env.FACEBOOK_CALLBACK_URL : 'http://localhost:3000/',
-  profileFields: ['email','id', 'first_name', 'gender', 'last_name', 'picture'],
-}, (accessToken, refreshToken, profile, done) => {
-  console.log(profile.id, profile.email, profile)
-  User.findOne({facebookId: profile.id}, (err, user) => {
-    console.log(user, 'ola <<<<')
-    if (err) return done(err)
-    if (user) return done(null, user)
-    else {
-      var newUser = new User()
-      newUser.facebookId = profile.id
-      newUser.name = profile.name.givenName
-      newUser.image = profile.photos[0].value
-      newUser.email = profile.emails[0].value
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL:
+        process.env.ON_PROD == "1"
+          ? process.env.FACEBOOK_CALLBACK_URL
+          : "http://localhost:3000/",
+      profileFields: [
+        "email",
+        "id",
+        "first_name",
+        "gender",
+        "last_name",
+        "picture",
+      ],
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile.id, profile.email, profile);
+      User.findOne({ facebookId: profile.id }, (err, user) => {
+        console.log(user, "ola <<<<");
+        if (err) return done(err);
+        if (user)
+          return done(null, {
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          });
+        else {
+          var newUser = new User();
+          newUser.facebookId = profile.id;
+          newUser.name = profile.name.givenName;
+          newUser.image = profile.photos[0].value;
+          newUser.email = profile.emails[0].value;
 
-      newUser.save((err) => {
-        if(err) throw err
-        return done(null, newUser)
-      })
+          newUser.save((err) => {
+            if (err) throw err;
+            return done(null, newUser);
+          });
+        }
+      });
     }
-  })
-}))
+  )
+);
 
-module.exports = passport
+module.exports = passport;
