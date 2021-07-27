b01fb382023a97a5ffe18c3b10c7013c3c8533c6
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-27 17:32:06 -0500

    add

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ const limiter = new RateLimit({
 
   // Esto resuelve el pedo con los cors
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', req.header('Origin'));
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
