a157dbbeabc260c7c9983c5a5b9428e283c7f093
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-26 22:51:02 -0500

    fix error porcess

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const FacebookStrategy = require('passport-facebook');
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: process.env.ON_PROD == '1' ? proccess.env.FACEBOOK_CALLBACK_URL : 'http://localhost:3000/'
+  callbackURL: process.env.ON_PROD == '1' ? process.env.FACEBOOK_CALLBACK_URL : 'http://localhost:3000/'
 }, (accessToken, refreshToken, profile, done) => {
   console.log(profile)
 }))
