13d61a86748daee8402745738f06259ded6c4c5b
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-07-27 16:32:25 -0500

    add

--- routes/index.js
+++ routes/index.js
@@ -71,7 +71,7 @@ router.post('/login', passport.authenticate('local'), async (req,res,next) =>{
   //   })(req, res, next)
   // });
 
-  router.get('/loginFacebook/callback', passport.authenticate('facebook', { successRedirect: '/',
+  router.get('/loginFacebook/callback', passport.authenticate('facebook', { successRedirect: '/profile',
   failureRedirect: process.env.FACEBOOK_CALLBACK_URL }))
 
   router.post('/logout', async (req,res,next) =>{
