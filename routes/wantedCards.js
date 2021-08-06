2e574ad3cf2ccfd3e94806f4601a3af1c2aea6b1
Oscar García Juárez <oscargarciajuarez@Oscar-Garcia-Juarez.local>
2021-08-06 01:25:31 -0500

    fix: delete console log

--- routes/wantedCards.js
+++ routes/wantedCards.js
@@ -14,21 +14,58 @@ router.post("/add", async (req, res, next) => {
         }
     })
     if (exist) {
-        await WantedCards.findOneAndUpdate({userId}, { $push: { cards: card } },)
+        await WantedCards.findOneAndUpdate({userId}, { $push: { cards: card }},)
     } else {
     await WantedCards.create({
       userId,
       email,
       name,
       cards: [card],
-      isFound: false
     });
 }
     res.status(200).json({ message: "Carta agregada" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Error al guardar el deck" });
+    res.status(500).json({ message: "Error al guardar la carta" });
   }
 });
+
+router.get("/getAllWantedCards", async (req, res , next) => {
+  try {
+    const allCards = await WantedCards.find();
+    const newCardsArr = allCards.map((user) => {
+      return user.cards.map((card) => {
+        return {
+          userId: user.userId ,
+          email: user.email,
+          name: user.name,
+          card: {image: card.image, rarityCode: card.rarityCode, name: card.name}
+        }
+      })
+    })
+    const allCardsArr = newCardsArr.flat()
+    res.status(200).json({allCards: allCardsArr})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({message: error})
+  }
+})
+
+router.put("/foundCard", async (req, res , next) => {
+  try {
+    const {userId, rarityCode} = req.body;
+    const card = await WantedCards.findOneAndUpdate({userId, 'cards.rarityCode': rarityCode}, 
+      {
+        $set: {
+          'cards.$.isFound': true,
+        }
+      },
+    )
+    res.status(200).json({message: 'Se ha notificado al usuario'})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({message: error})
+  }
+})
 
 module.exports = router;
